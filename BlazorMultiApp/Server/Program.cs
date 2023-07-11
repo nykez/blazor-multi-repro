c939eb35d6891962cce112fec3eaca7f44eeb1ec
Nykez <nykezgaming@live.com>
2023-07-11 18:40:35 -0400

    Initial commit

---
+++ BlazorMultiApp/Server/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+
+
+app.MapWhen(ctx => ctx.Request.Path.StartsWithSegments("/app2"), first =>
+{
+
+    first.UseBlazorFrameworkFiles("/app2");
+    first.UseStaticFiles();
+    first.UseStaticFiles("/app2");
+    first.UseRouting();
+
+    first.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+        endpoints.MapFallbackToFile("/app2/{*path:nonfile}",
+            "app2/index.html");
+    });
+});
+
+app.UseRouting();
+
+
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
